89a3fc7b46f539dedf71cc5698a177b21129ce24
Horacio Biancardi <50308943+HoracioBiancardi@users.noreply.github.com>
2024-06-02 15:42:02 -0300

    teste pipeline cobertura correta

--- tests/test_api.py
+++ tests/test_api.py
@@ -17,7 +17,7 @@ def test_read_main():
     assert response.json() == {"message": "Hello, World"}
 
 
-# def test_read_item():
-#     response = client.get("/items/1")
-#     assert response.status_code == 200
-#     assert response.json() == {"item_id": 1, "q": None}
+def test_read_item():
+    response = client.get("/items/1")
+    assert response.status_code == 200
+    assert response.json() == {"item_id": 1, "q": None}
