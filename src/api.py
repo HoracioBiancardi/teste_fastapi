3de04138aee04b18e75aefc600a11cc253a3c063
Horacio Biancardi <50308943+HoracioBiancardi@users.noreply.github.com>
2024-06-02 18:04:15 -0300

    docstring

--- src/api.py
+++ src/api.py
@@ -5,10 +5,24 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+def read_root() -> dict:
+    """Rota principal
+
+    Returns:
+        dict: Hello, World
+    """
     return {"message": "Hello, World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
+def read_item(item_id: int, q: str = None) -> dict:
+    """Rota de ge items
+
+    Args:
+        item_id (int): id do item
+        q (str, optional): _description_. Defaults to None.
+
+    Returns:
+        dict: item_id
+    """
     return {"item_id": item_id, "q": q}
